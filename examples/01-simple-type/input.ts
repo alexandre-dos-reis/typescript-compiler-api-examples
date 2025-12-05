0d44373b1520be9d5f4c55d828d32e959bf6717a
Alexandre Dos Reis <ajm.dosreis.daponte@gmail.com>
2025-12-05 14:23:36 +0100

    simple example

---
+++ examples/01-simple-type/input.ts
@@ -0,0 +1,41 @@
+import ts from "typescript";
+import { writeFile } from "node:fs/promises";
+import { join } from "node:path";
+
+const userTypeNode = ts.factory.createTypeAliasDeclaration(
+  [ts.factory.createModifier(ts.SyntaxKind.ExportKeyword)],
+  ts.factory.createIdentifier("User"),
+  undefined,
+  ts.factory.createTypeLiteralNode([
+    ts.factory.createPropertySignature(
+      undefined,
+      "id",
+      undefined,
+      ts.factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
+    ),
+    ts.factory.createPropertySignature(
+      undefined,
+      "name",
+      undefined,
+      ts.factory.createKeywordTypeNode(ts.SyntaxKind.StringKeyword),
+    ),
+    ts.factory.createPropertySignature(
+      undefined,
+      "age",
+      ts.factory.createToken(ts.SyntaxKind.QuestionToken),
+      ts.factory.createKeywordTypeNode(ts.SyntaxKind.NumberKeyword),
+    ),
+  ]),
+);
+
+const file = ts.factory.createSourceFile(
+  [userTypeNode],
+  ts.factory.createToken(ts.SyntaxKind.EndOfFileToken),
+  ts.NodeFlags.None,
+);
+
+const printer = ts.createPrinter();
+
+const output = printer.printFile(file);
+
+await writeFile(join(import.meta.dir, "output.ts"), output);
